dd98f12a421003b9e0dad228708335947eaf47c6
Dinesh <dinesht.bits@gmail.com>
2016-06-05 01:38:28 -0500

    attr accessor to reader

--- roman_number_converter.rb
+++ roman_number_converter.rb
@@ -1,5 +1,5 @@
 class RomanNumberConverter
-  attr_accessor :roman_map
+  attr_reader :roman_map
 
   def initialize
     @roman_map = { 1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000 => "M" }
